9ec2d4781fa126da34ad587d376df5b609eea27f
Michel Fonseca <michelbeserradafonseca@gmail.com>
2022-10-19 10:40:29 -0300

    added debuger

--- src/server.ts
+++ src/server.ts
@@ -2,8 +2,16 @@ import express from "express";
 
 const app = express();
 
+app.use(express.json());
+
 app.get("/", (request, response) => {
   response.json({ message: "Server is Ok@" });
+});
+
+app.post("/courses", (request, response) => {
+  const { name } = request.body;
+
+  return response.json({ name });
 });
 
 app.listen(3333, () => console.log("Server is listening at port 3333"));
